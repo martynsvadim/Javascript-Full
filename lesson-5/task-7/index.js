dbae804339a2454513aea2acdc795eecd7784cc9
martynsvadim <martynenkovadim3@gmail.com>
2023-01-24 00:03:53 +0200

    homework 3

---
+++ lesson-5/task-7/index.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number} from
+ * @param {number} to
+ * @return {number}
+ */
+function sum(from, to) {
+  let result = 0;
+  for (let i = from; i <= to; i += 1) {
+    result += i;
+  }
+  return result;
+}
+
+/**
+ * @param {number} firstFrom
+ * @param {number} firstTo
+ * @param {number} secondFrom
+ * @param {number} secondTo
+ * @return {boolean}
+ */
+function compareSums(firstFrom, firstTo, secondFrom, secondTo) {
+  if (sum(firstFrom, firstTo) > sum(secondFrom, secondTo)) {
+    return true;
+  }
+  return false;
+}
+
+// examples
+console.log(sum(5, 10)); // ===> 45
+console.log(sum(11, 11)); // ===> 11
+
+console.log(compareSums(5, 10, 5, 10)); // ===> false
+console.log(compareSums(5, 15, 3, 5)); // ===> true
+console.log(compareSums(-2, 5, 5, 9)); // ===> false
