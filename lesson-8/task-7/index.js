c714069294b2a8f0423105d4a965adf2ca373cef
martynsvadim <martynenkovadim3@gmail.com>
2023-02-03 11:53:46 +0200

    lesson 8 full js

---
+++ lesson-8/task-7/index.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-restricted-syntax */
+
+/* Для решения этой задачи используй for..in цикл. В реальных проектах это плохая практика,
+ * лучше использовать методы класса Object - keys(), values(), entries(). Но мы с ними пока не познакомитись.
+ * Чтобы eslint не ругался на эту ошибку, для этой задачи он отключен аннотацией eslint-disable
+ * */
+
+const getAdults = (obj) => {
+  const copyObj = {};
+
+  for (let key in obj) obj[key] >= 18 ? (copyObj[key] = obj[key]) : copyObj;
+
+  return copyObj;
+};
+
+// examples
+console.log(getAdults({ 'John Doe': 19, Tom: 17, Bob: 18 })); // ==> { 'John Doe': 19, Bob: 18 }
+console.log(getAdults({ Ann: 56, Andrey: 7 })); // ==> { Ann: 56 }
